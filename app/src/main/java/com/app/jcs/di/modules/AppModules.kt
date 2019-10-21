64ad22403471ba9d0778fd82f2043178ef025085
Anant00 <anantpundir02@gmail.com>
2019-10-21 17:43:15 +0530

    optimise imports

--- app/src/main/java/com/app/jcs/di/modules/AppModules.kt
+++ app/src/main/java/com/app/jcs/di/modules/AppModules.kt
@@ -1,14 +1,15 @@
 package com.app.jcs.di.modules
 
-import com.app.jcs.api.retrofitBuilder.RetrofitBuilder
+import com.app.jcs.api.retrofitBuilder.RetrofitBuilder.provideOkHttpClient
+import com.app.jcs.api.retrofitBuilder.RetrofitBuilder.provideRetrofitInstance
 import com.app.jcs.di.providers.ModuleProviders.provideAuthViewModules
 import com.app.jcs.di.providers.ModuleProviders.provideCompositeDisposable
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 var retrofitModule = module {
-    single { RetrofitBuilder.provideOkHttpClient() }
-    single { RetrofitBuilder.provideRetrofitInstance(get()) }
+    single { provideOkHttpClient() }
+    single { provideRetrofitInstance(get()) }
 }
 
 var authViewModule = module {
