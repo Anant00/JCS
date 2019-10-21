86ca8c9457e74df065ae7ed63c76e901fd6179af
Anant00 <anantpundir02@gmail.com>
2019-10-21 13:15:17 +0530

    koin setup

--- app/src/main/java/com/app/jcs/view/activities/MainActivity.kt
+++ app/src/main/java/com/app/jcs/view/activities/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.app.jcs.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.app.jcs.R
 
 class MainActivity : AppCompatActivity() {
-
+    private val tag by lazy {
+        javaClass.simpleName
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
